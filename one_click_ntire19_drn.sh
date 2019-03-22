a4001be5d9d0f3e7fe14834bbce263a078f91317
tangwy's nuc <twytwy12345@live.com>
2019-03-22 22:06:06 +0800

    change into absolute path

--- one_click_ntire19_drn.sh
+++ one_click_ntire19_drn.sh
@@ -3,13 +3,14 @@
 # Date: Mar. 18 2019
 # Email: wenyitang@outlook.com
 
+
 #DRN_TEST_MAT=
 #DRN_SAVE_DIR=
-if [ ${DRN_SAVE_DIR:0:1} == '/' ];
-then
-  _ABS=1
-else
-  _ABS=0
+if [ ${DRN_TEST_MAT:0:1} == '/' ];then
+  DRN_TEST_MAT=`pwd`/${DRN_TEST_MAT}
+fi
+if [ ${DRN_SAVE_DIR:0:1} == '/' ];then
+  DRN_SAVE_DIR=`pwd`/${DRN_SAVE_DIR}
 fi
 
 if [ ! -e VideoSuperResolution ];
@@ -17,13 +18,7 @@ then
   git clone https://github.com/LoSealL/VideoSuperResolution -b ntire_2019
 fi
 
-pushd VideoSuperResolution
-if [ ${_ABS} == 0 ];
-then
-  DRN_SAVE_DIR=../${DRN_SAVE_DIR}
-  DRN_TEST_MAT=../${DRN_TEST_MAT}
-fi
-if 
+pushd VideoSuperResolution 
 if [ ! -e setup.py ];
 then
   echo " [!] Can't find setup.py file! Make sure you are in the right place!"
@@ -37,27 +32,9 @@ python prepare_data.py --filter=drn -q
 echo " [*] Model extracted into Results/drn/save"
 python VSR/Tools/DataProcessing/NTIRE19Denoise.py --validation=${DRN_TEST_MAT} --save_dir=${DRN_SAVE_DIR}/1/
 pushd VSRTorch
-if [ ${_ABS} == 0 ];
-then
-  DRN_SAVE_DIR=../${DRN_SAVE_DIR}
-  DRN_TEST_MAT=../${DRN_TEST_MAT}
-  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
-fi
 python eval.py drn --cuda --ensemble -t=${DRN_SAVE_DIR}/1/ --output_index=0
 popd
-if [ ${_ABS} == 0 ];
-then
-  DRN_SAVE_DIR=${DRN_SAVE_DIR:3}
-  DRN_TEST_MAT=${DRN_TEST_MAT:3}
-  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
-fi
 python VSR/Tools/DataProcessing/NTIRE19Denoise.py --results=Results/drn/1/ --save_dir=${DRN_SAVE_DIR}/2/
 echo " [*] Processing done. Results are in ${DRN_SAVE_DIR}/2/results.mat"
 echo "     PNG files are saved in VideoSuperResolution/Results/drn/1/"
 popd
-if [ ${_ABS} == 0 ];
-then
-  DRN_SAVE_DIR=${DRN_SAVE_DIR:3}
-  DRN_TEST_MAT=${DRN_TEST_MAT:3}
-  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
-fi
--- one_click_ntire19_rsr.sh
+++ one_click_ntire19_rsr.sh
@@ -5,11 +5,11 @@
 
 #RSR_TEST_DIR=
 #RSR_SAVE_DIR=
-if [ ${RSR_SAVE_DIR:0:1} == '/' ];
-then
-  _ABS=1
-else
-  _ABS=0
+if [ ${RSR_TEST_DIR:0:1} == '/' ];then
+  RSR_TEST_DIR=`pwd`/${RSR_TEST_DIR}
+fi
+if [ ${RSR_SAVE_DIR:0:1} == '/' ];then
+  RSR_SAVE_DIR=`pwd`/${RSR_SAVE_DIR}
 fi
 if [ -z ${_PATCH_SIZE} ];
 then
@@ -26,11 +26,6 @@ then
 fi
 
 pushd VideoSuperResolution
-if [ ${_ABS} == 0 ];
-then
-  RSR_SAVE_DIR=../${RSR_SAVE_DIR}
-  RSR_TEST_DIR=../${RSR_TEST_DIR}
-fi
 if [ ! -e setup.py ];
 then
   echo " [!] Can't find setup.py file! Make sure you are in the right place!"
@@ -45,26 +40,8 @@ python prepare_data.py --filter=rsr -q
 echo " [*] Model extracted into Results/rsr/save"
 python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --save_dir=${RSR_SAVE_DIR}/1/
 pushd VSRTorch
-if [ ${_ABS} == 0 ];
-then
-  RSR_SAVE_DIR=../${RSR_SAVE_DIR}
-  RSR_TEST_DIR=../${RSR_TEST_DIR}
-  echo "RSR_SAVE_DIR=${RSR_SAVE_DIR}"
-fi
 python eval.py rsr --cuda --ensemble -t=${RSR_SAVE_DIR}/1/
 popd
-if [ ${_ABS} == 0 ];
-then
-  RSR_SAVE_DIR=${RSR_SAVE_DIR:3}
-  RSR_TEST_DIR=${RSR_TEST_DIR:3}
-  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
-fi
 python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --results=Results/rsr/1/ --save_dir=${RSR_SAVE_DIR}/2/
 echo " [*] Processing done. Results are in ${RSR_SAVE_DIR}/2/"
 popd
-if [ ${_ABS} == 0 ];
-then
-  RSR_SAVE_DIR=${RSR_SAVE_DIR:3}
-  RSR_TEST_DIR=${RSR_TEST_DIR:3}
-  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
-fi
