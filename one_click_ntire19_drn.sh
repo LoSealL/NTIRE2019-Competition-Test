ca7b30169b887dea3651bf0dbf74bad722be8579
tangwy's nuc <twytwy12345@live.com>
2019-03-22 22:13:56 +0800

    Fix typo error

--- one_click_ntire19_drn.sh
+++ one_click_ntire19_drn.sh
@@ -3,14 +3,13 @@
 # Date: Mar. 18 2019
 # Email: wenyitang@outlook.com
 
-
-#DRN_TEST_MAT=
-#DRN_SAVE_DIR=
-if [ ${DRN_TEST_MAT:0:1} == '/' ];then
-  DRN_TEST_MAT=`pwd`/${DRN_TEST_MAT}
+_DRN_TEST_MAT=${DRN_TEST_MAT}
+_DRN_SAVE_DIR=${DRN_SAVE_DIR}
+if [ ${DRN_TEST_MAT:0:1} != '/' ];then
+  _DRN_TEST_MAT=`pwd`/${DRN_TEST_MAT}
 fi
-if [ ${DRN_SAVE_DIR:0:1} == '/' ];then
-  DRN_SAVE_DIR=`pwd`/${DRN_SAVE_DIR}
+if [ ${DRN_SAVE_DIR:0:1} != '/' ];then
+  _DRN_SAVE_DIR=`pwd`/${DRN_SAVE_DIR}
 fi
 
 if [ ! -e VideoSuperResolution ];
@@ -24,17 +23,17 @@ then
   echo " [!] Can't find setup.py file! Make sure you are in the right place!"
 fi
 
-echo "DRN_TEST_MAT=${DRN_TEST_MAT}"
-echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
+echo "DRN_TEST_MAT=${_DRN_TEST_MAT}"
+echo "DRN_SAVE_DIR=${_DRN_SAVE_DIR}"
 
 pip install -q -e .
 python prepare_data.py --filter=drn -q
 echo " [*] Model extracted into Results/drn/save"
-python VSR/Tools/DataProcessing/NTIRE19Denoise.py --validation=${DRN_TEST_MAT} --save_dir=${DRN_SAVE_DIR}/1/
+python VSR/Tools/DataProcessing/NTIRE19Denoise.py --validation=${_DRN_TEST_MAT} --save_dir=${_DRN_SAVE_DIR}/1/
 pushd VSRTorch
-python eval.py drn --cuda --ensemble -t=${DRN_SAVE_DIR}/1/ --output_index=0
+python eval.py drn --cuda --ensemble -t=${_DRN_SAVE_DIR}/1/ --output_index=0
 popd
-python VSR/Tools/DataProcessing/NTIRE19Denoise.py --results=Results/drn/1/ --save_dir=${DRN_SAVE_DIR}/2/
-echo " [*] Processing done. Results are in ${DRN_SAVE_DIR}/2/results.mat"
+python VSR/Tools/DataProcessing/NTIRE19Denoise.py --results=Results/drn/1/ --save_dir=${_DRN_SAVE_DIR}/2/
+echo " [*] Processing done. Results are in ${_DRN_SAVE_DIR}/2/results.mat"
 echo "     PNG files are saved in VideoSuperResolution/Results/drn/1/"
 popd
--- one_click_ntire19_rsr.sh
+++ one_click_ntire19_rsr.sh
@@ -3,13 +3,13 @@
 # Date: Mar. 18 2019
 # Email: wenyitang@outlook.com
 
-#RSR_TEST_DIR=
-#RSR_SAVE_DIR=
-if [ ${RSR_TEST_DIR:0:1} == '/' ];then
-  RSR_TEST_DIR=`pwd`/${RSR_TEST_DIR}
+_RSR_TEST_DIR=${RSR_TEST_DIR}
+_RSR_SAVE_DIR=${RSR_SAVE_DIR}
+if [ ${RSR_TEST_DIR:0:1} != '/' ];then
+  _RSR_TEST_DIR=`pwd`/${RSR_TEST_DIR}
 fi
-if [ ${RSR_SAVE_DIR:0:1} == '/' ];then
-  RSR_SAVE_DIR=`pwd`/${RSR_SAVE_DIR}
+if [ ${RSR_SAVE_DIR:0:1} != '/' ];then
+  _RSR_SAVE_DIR=`pwd`/${RSR_SAVE_DIR}
 fi
 if [ -z ${_PATCH_SIZE} ];
 then
@@ -31,17 +31,17 @@ then
   echo " [!] Can't find setup.py file! Make sure you are in the right place!"
 fi
 
-echo "RSR_TEST_DIR=${RSR_TEST_DIR}"
-echo "RSR_SAVE_DIR=${RSR_SAVE_DIR}"
+echo "RSR_TEST_DIR=${_RSR_TEST_DIR}"
+echo "RSR_SAVE_DIR=${_RSR_SAVE_DIR}"
 echo "Patch size=${_PATCH_SIZE}, stride=${_STRIDE}"
 
 pip install -q -e .
 python prepare_data.py --filter=rsr -q
 echo " [*] Model extracted into Results/rsr/save"
-python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --save_dir=${RSR_SAVE_DIR}/1/
+python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${_RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --save_dir=${_RSR_SAVE_DIR}/1/
 pushd VSRTorch
-python eval.py rsr --cuda --ensemble -t=${RSR_SAVE_DIR}/1/
+python eval.py rsr --cuda --ensemble -t=${_RSR_SAVE_DIR}/1/
 popd
-python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --results=Results/rsr/1/ --save_dir=${RSR_SAVE_DIR}/2/
-echo " [*] Processing done. Results are in ${RSR_SAVE_DIR}/2/"
+python VSR/Tools/DataProcessing/NTIRE19RSR.py --ref_dir=${_RSR_TEST_DIR} --patch_size=${_PATCH_SIZE} --stride=${_STRIDE} --results=Results/rsr/1/ --save_dir=${_RSR_SAVE_DIR}/2/
+echo " [*] Processing done. Results are in ${_RSR_SAVE_DIR}/2/"
 popd
