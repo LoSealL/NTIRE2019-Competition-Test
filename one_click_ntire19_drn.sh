a8e495f8581e90515fd57fad3a401647017388be
tangwy's nuc <twytwy12345@live.com>
2019-03-22 21:52:26 +0800

    update

--- one_click_ntire19_drn.sh
+++ one_click_ntire19_drn.sh
@@ -5,6 +5,12 @@
 
 #DRN_TEST_MAT=
 #DRN_SAVE_DIR=
+if [ ${DRN_SAVE_DIR:0:1} == '/' ];
+then
+  _ABS=1
+else
+  _ABS=0
+fi
 
 if [ ! -e VideoSuperResolution ];
 then
@@ -12,6 +18,12 @@ then
 fi
 
 pushd VideoSuperResolution
+if [ ${_ABS} == 0 ];
+then
+  DRN_SAVE_DIR=../${DRN_SAVE_DIR}
+  DRN_TEST_MAT=../${DRN_TEST_MAT}
+fi
+if 
 if [ ! -e setup.py ];
 then
   echo " [!] Can't find setup.py file! Make sure you are in the right place!"
@@ -25,9 +37,27 @@ python prepare_data.py --filter=drn -q
 echo " [*] Model extracted into Results/drn/save"
 python VSR/Tools/DataProcessing/NTIRE19Denoise.py --validation=${DRN_TEST_MAT} --save_dir=${DRN_SAVE_DIR}/1/
 pushd VSRTorch
+if [ ${_ABS} == 0 ];
+then
+  DRN_SAVE_DIR=../${DRN_SAVE_DIR}
+  DRN_TEST_MAT=../${DRN_TEST_MAT}
+  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
+fi
 python eval.py drn --cuda --ensemble -t=${DRN_SAVE_DIR}/1/ --output_index=0
 popd
+if [ ${_ABS} == 0 ];
+then
+  DRN_SAVE_DIR=${DRN_SAVE_DIR:3}
+  DRN_TEST_MAT=${DRN_TEST_MAT:3}
+  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
+fi
 python VSR/Tools/DataProcessing/NTIRE19Denoise.py --results=Results/drn/1/ --save_dir=${DRN_SAVE_DIR}/2/
 echo " [*] Processing done. Results are in ${DRN_SAVE_DIR}/2/results.mat"
 echo "     PNG files are saved in VideoSuperResolution/Results/drn/1/"
 popd
+if [ ${_ABS} == 0 ];
+then
+  DRN_SAVE_DIR=${DRN_SAVE_DIR:3}
+  DRN_TEST_MAT=${DRN_TEST_MAT:3}
+  echo "DRN_SAVE_DIR=${DRN_SAVE_DIR}"
+fi
